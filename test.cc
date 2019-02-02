71904aa80ed50e96211decae97da22d55d8d5411
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2019-02-02 11:48:40 -0500

    Update test.cc

--- test.cc
+++ test.cc
@@ -3,8 +3,8 @@
 #include "test.h"
 
 // make sure that the file path/dir information below is correct
-const char *dbfile_dir = ""; // dir where binary heap files should be stored
-const char *tpch_dir ="/cise/tmp/dbi_sp11/DATA/10M/"; // dir where dbgen tpch files (extension *.tbl) can be found
+const char *dbfile_dir = "./"; // dir where binary heap files should be stored
+const char *tpch_dir ="./"; // dir where dbgen tpch files (extension *.tbl) can be found
 const char *catalog_path = "catalog"; // full path of the catalog file
 
 using namespace std;
