44e42787e425a83f7a804e373e923eea0bb51bfa
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2019-02-02 11:47:52 -0500

    Update HeapFile.cc

--- HeapFile.cc
+++ HeapFile.cc
@@ -15,76 +15,73 @@ HeapFile::~HeapFile() {
 }
 
 
-int HeapFile::Create(char *f_path, fType f_type, void *startup) {
+int HeapFile::Create(const char *f_path, void *startup) {
 	mainFile = new File();
-	mainFile->Open(0, f_path);
+	mainFile->Open(0, (char *)f_path);
 	bufferPage = new Page();
 	readPagePointer = new Page();
 	currentPageNumber = 0;
+	isBufferEmpty = 1;
 	return 1;
 }
 
-int HeapFile::Open(char *f_path) {
+int HeapFile::Open(const char *f_path) {
 	mainFile = new File();
-	mainFile->Open(1, f_path);
+	mainFile->Open(1, (char *)f_path);
 	bufferPage = new Page();
 	readPagePointer = new Page();
-	mainFile->GetPage(readPagePointer,0);
+	if (mainFile->GetLength() > 0) {
+		mainFile->GetPage(readPagePointer, 0);
+	}
 	currentPageNumber = 0;
+	isBufferEmpty = 1;
 	return 1;
 }
 
-void HeapFile::Load(Schema &f_schema, char *loadpath) {
+void HeapFile::Load(Schema &f_schema, const char *loadpath) {
 	FILE *fileToRead = fopen(loadpath, "r");
-	Record tempRecord;
 	
+	Record tempRecord;
 	while (tempRecord.SuckNextRecord(&f_schema, fileToRead) == 1) {
 		Add(tempRecord);
 	}
 
-	int totalPages = mainFile->GetLength();
-	totalPages = (totalPages == 0) ? 0 : totalPages - 1;
-	mainFile->AddPage(bufferPage, totalPages);
+	if (isBufferEmpty == 0) {
+		int totalPages = mainFile->GetLength();
+		totalPages = (totalPages == 0) ? 0 : totalPages - 1;
+		mainFile->AddPage(bufferPage, totalPages);
+		bufferPage->EmptyItOut();
+		isBufferEmpty = 1;
+	}
 
-	cout << "Load done. Number of Pages: " << mainFile->GetLength() << endl;
+	//cout << "Load done. Number of Pages: " << mainFile->GetLength() << endl;
 	fclose(fileToRead);
 }
 
 void HeapFile::MoveFirst() {
 	off_t totalNumOfPages = mainFile->GetLength();
 	if (totalNumOfPages == 0) {
-		mainFile->AddPage(bufferPage, 0);
-		bufferPage->EmptyItOut();
+		if (isBufferEmpty == 0) {
+			mainFile->AddPage(bufferPage, 0);
+			bufferPage->EmptyItOut();
+			isBufferEmpty = 1;
+		}
+		else {
+			readPagePointer = new Page();
+			currentPageNumber = 0;
+			return;
+		}
 	}
 
 	mainFile->GetPage(readPagePointer, 0);
-	
-	//Record rec;
-	//temp.GetFirst(&rec);
-	//recordPointer = &rec;
 	currentPageNumber = 0;
-
-	//TODO
-	// Remove test code
-	/*
-	Schema mySchema("catalog", "lineitem");
-	(rec).Print(&mySchema);
-	cout << endl;
-	temp.GetFirst(&rec);
-	(rec).Print(&mySchema);
-	cout << endl;
-
-	Record rec1;
-	temp.GetFirst(&rec1);
-	(rec1).Print(&mySchema);
-	cout << endl;
-	*/
 }
 
 void HeapFile::Add(Record &rec) {
 	int result = bufferPage->Append(&rec);
 
 	if (result == 1) {
+		isBufferEmpty = 0;
 		return;
 	}
 	else {
@@ -97,10 +94,12 @@ void HeapFile::Add(Record &rec) {
 			currentPageNumber = 0;
 		}
 		bufferPage->EmptyItOut();
+		isBufferEmpty = 1;
 		//delete bufferPage;
 		//bufferPage = NULL;
 		//bufferPage = new Page();
 		bufferPage->Append(&rec);
+		isBufferEmpty = 0;
 	}
 }
 
@@ -115,18 +114,15 @@ int HeapFile::GetNext(Record &fetchme) {
 	off_t totalPagesInDisk = mainFile->GetLength();
 	// Case2 - Part A : No pages in Disk
 	if (totalPagesInDisk == 0) {
-		mainFile->AddPage(bufferPage, 0);
-		bufferPage->EmptyItOut();
-		
-		mainFile->GetPage(readPagePointer, 0);
-		currentPageNumber = 0;
+		if (isBufferEmpty == 0) {
+			mainFile->AddPage(bufferPage, 0);
+			bufferPage->EmptyItOut();
+			isBufferEmpty = 1;
 
-		int result = readPagePointer->GetFirst(&fetchme);
-		if (result == 1) {
-			return 1;
-		}
-		else {
-			return 0;
+			mainFile->GetPage(readPagePointer, 0);
+			currentPageNumber = 0;
+
+			return readPagePointer->GetFirst(&fetchme);
 		}
 	}
 	// Case2 - Part B : Pages available in disk
@@ -136,34 +132,28 @@ int HeapFile::GetNext(Record &fetchme) {
 		// Case2 - Part B - SubPart 1: totalPagesInDisk-1 > currentPageNumber
 		if (totalPagesInDisk - 1 > currentPageNumber) {
 			mainFile->GetPage(readPagePointer,currentPageNumber);
-			readPagePointer->GetFirst(&fetchme);
-			return 1;
+			return readPagePointer->GetFirst(&fetchme);
 		} 
 		// Case2 - Part B - SubPart 2: totalPagesInDisk -1 == currentPageNumber
 		else if (totalPagesInDisk - 1 == currentPageNumber) {
-			mainFile->AddPage(bufferPage, currentPageNumber);
-			bufferPage->EmptyItOut();
+			if (isBufferEmpty == 0) {
+				mainFile->AddPage(bufferPage, currentPageNumber);
+				bufferPage->EmptyItOut();
+				isBufferEmpty = 1;
 
-			mainFile->GetPage(readPagePointer, currentPageNumber);
-
-			int result = readPagePointer->GetFirst(&fetchme);
-			if (result == 1) {
-				return 1;
-			}
-			else {
-				return 0;
+				mainFile->GetPage(readPagePointer, currentPageNumber);
+				return readPagePointer->GetFirst(&fetchme);
 			}
 		}
 	}
-	// Worst Case
 	return 0;
 
 }
 
 int HeapFile::GetNext(Record &fetchme, CNF &cnf, Record &literal) {
 	ComparisonEngine comp;
-	Page temp;
-	temp = (*readPagePointer);
+	Page *temp = new Page();
+	temp = readPagePointer;
 	int tempPageNumber = currentPageNumber;
 
 	while (GetNext(fetchme)) {
@@ -175,25 +165,27 @@ int HeapFile::GetNext(Record &fetchme, CNF &cnf, Record &literal) {
 		}
 	}
 
-	*readPagePointer = temp;
+	readPagePointer = temp;
 	currentPageNumber = tempPageNumber;
-
+	temp = NULL;
 	return 0;
 }
 
 int HeapFile::Close() {
-	//TODO
-	// Check if working
-	int totalPages = mainFile->GetLength();
-	totalPages = (totalPages == 0) ? 0 : totalPages - 1;
-	mainFile->AddPage(bufferPage, totalPages);
-
+	if (isBufferEmpty == 0) {
+		int totalPages = mainFile->GetLength();
+		totalPages = (totalPages == 0) ? 0 : totalPages - 1;
+		mainFile->AddPage(bufferPage, totalPages);
+		bufferPage->EmptyItOut();
+		isBufferEmpty = 1;
+	}
 	mainFile->Close();
 	delete mainFile;
 	mainFile = NULL;
 	delete bufferPage;
 	bufferPage = NULL;
 	readPagePointer = NULL;
+	isBufferEmpty = 1;
 	currentPageNumber = 0;
 	return 1;
 }
