dfef07ba60f7ac958eb2f4ee83af2da02fd9d687
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2019-02-02 12:48:54 -0500

    Update DBFile.h

--- DBFile.h
+++ DBFile.h
@@ -9,6 +9,8 @@
 #include "ComparisonEngine.h"
 #include "RawDBFile.h"
 
+typedef enum { heap, sorted, tree } fType;
+
 class DBFile {
 private:
 	RawDBFile *rawDBFile;
@@ -16,11 +18,11 @@ private:
 public:
 	DBFile (); 
 	~DBFile();
-	int Create (char *fpath, fType file_type, void *startup);
-	int Open (char *fpath);
+	int Create (const char *fpath, fType file_type, void *startup);
+	int Open (const char *fpath);
 	int Close ();
 
-	void Load (Schema &myschema, char *loadpath);
+	void Load (Schema &myschema, const char *loadpath);
 
 	void MoveFirst ();
 	void Add (Record &addme);
